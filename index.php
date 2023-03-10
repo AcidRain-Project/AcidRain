31cc554e2d1ed332efa07a8c78cb867f76208141
114soyeon <kim971104@naver.com>
2023-03-10 14:59:47 +0900

    230310_수정

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ $ContentAcidRainActivityWord = array_slice($ContentAcidRainActivityWord,0,$Conte
     <div class="save_wrap" style="display: none;">
         <div class="save_area">
             <div class="save_img">
-                <img class="save_img_class" src="" style="display: ">
+                <img class="save_img_class" src="" style="display: ;">
             </div>
             <div class="save_btns">
                 <button>Retry</button>
